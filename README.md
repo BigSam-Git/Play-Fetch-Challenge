1bb684ac9e3f9f90ab2acdd1f2745485ab437f51
Samuel Shonowo <sammuelshonowo10@gmail.com>
2022-09-24 10:41:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# play-fetch-challenge-2
+ Play-fetch. Unity challenge 2 submission
---
+++ Scripts/DestroyOutOfBoundsX.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBoundsX : MonoBehaviour
+{
+    private float leftLimit = -30;
+    private float bottomLimit = -5f;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Destroy dogs if x position less than left limit and ball if z position less than bottom limit
+        if (transform.position.x < leftLimit)
+        {
+            Destroy(gameObject);
+        } else if (transform.position.y < bottomLimit)
+        {
+            Debug.Log("Game Over");
+            Destroy(gameObject);
+        }
+
+    }
+}
